b9c50755a92aba2e2f2b15a3426d23905b6c278b
Jennifer Sadler <jennifer.sadler@gmail.com>
2024-07-04 14:50:44 -0400

    removed irrelevant branches

--- server.js
+++ server.js
@@ -21,26 +21,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/gross-op-expend', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'gross-op-expend.html'));
-});
-
-app.get('/other-revenues', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'other-revenues.html'));
-});
-
-app.get('/levy-supported-operating', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'levy-supported-operating.html'));
-});
-
-app.get('/total-cap-expenditures', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'total-cap-expenditures.html'));
-});
-
-app.get('/fte', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'fte.html'));
-});
-
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
